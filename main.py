fdeea2cbe60db101c5809d1450b694bc7e32d10b
Aleksei Kudinov <reanim86@yandex.ru>
2022-08-24 21:04:42 +0300

    Add file name to disk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 class YaUploader:
     def __init__(self, token: str):
@@ -15,35 +14,15 @@ class YaUploader:
         """Метод загружает файлы по списку file_list на яндекс диск"""
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.my_headers()
-        params = {'path': f'{file_path}', 'overwrite': 'true'}
+        file_to_disk = file_path.split('/')
+        params = {'path': f'{file_to_disk[-1]}', 'overwrite': 'true'}
         get_link = requests.get(url, headers=headers, params=params)
         link = get_link.json()
-        response = requests.put(url=link['href'], data=open('./upload/text.txt'))
-        pprint(response)
-
-
+        response = requests.put(url=link['href'], data=open(file_path))
+        response.raise_for_status()
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = 'test.txt'
+    path_to_file = './upload/text.txt'
     token = ''
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
-
-
-
-
-
-
-# url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-# token = ''
-# headers = {
-#     'Content-Type': 'application/json',
-#     'Authorization': 'OAuth '
-# }
-# params = {'path': 'test.txt', 'overwrite': 'true'}
-# get_link = requests.get(url, headers=headers, params=params)
-# link = get_link.json()
-# response = requests.put(url=link['href'], data=open('./upload/text.txt'))
-#
-# pprint(response)
